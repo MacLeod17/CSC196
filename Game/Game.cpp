61797421a4cd8975627b9e764b21acc323d6d60a
Garrick Kilpack <garrickkilpack@hotmail.com>
2020-06-29 18:11:42 -0600

    Set up the Middleware stuff, added Core to the Game project

--- Game/Game.cpp
+++ Game/Game.cpp
@@ -1,8 +1,28 @@
 
+#include "core.h"
 #include <iostream>
+
+bool Update(float dt)
+{
+	return false;
+}
+
+void Draw(Core::Graphics& graphics)
+{
+	graphics.SetColor(RGB(rand() % 256, rand() % 256, rand() % 256));
+	graphics.DrawLine(static_cast<float>(rand() % 800), static_cast<float>(rand() % 600), static_cast<float>(rand() % 800), static_cast<float>(rand() % 600));
+}
 
 int main()
 {
+	char name[] = "Kilpack";
+	Core::Init(name, 800, 600);
+	Core::RegisterUpdateFn(Update);
+	Core::RegisterDrawFn(Draw);
+
+	Core::GameLoop();
+	Core::Shutdown();
+
     std::cout << "Hello World!\n";
 }
 
