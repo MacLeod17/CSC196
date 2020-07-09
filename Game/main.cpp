95bbb07451c54ce1c9f5bf0c8639226afb9a7343
Garrick Kilpack <garrickkilpack@hotmail.com>
2020-07-09 16:51:35 -0600

    Changes from 7/9 class

--- Game/main.cpp
+++ Game/main.cpp
@@ -11,6 +11,8 @@
 gk::Shape shape;
 gk::Transform transform{ { 400, 300 }, 4, 0 };
 
+float t{ 0 };
+
 float speed = 300.0f;
 float frametime;
 float roundtime{ 0 };
@@ -25,6 +27,8 @@ bool Update(float dt) // dt = Delta Time
 	deltatime = time - prevtime; //Current frame time / Previous frame time
 	prevtime = time;
 	
+	t += dt * 3.0f;
+
 	frametime = dt;
 	roundtime += dt;
 
@@ -47,9 +51,11 @@ bool Update(float dt) // dt = Delta Time
 	transform.position += direction;
 
 	//Rotate
-	if (Core::Input::IsPressed('A')) { transform.angle -= (dt * 3.0f); }
-	if (Core::Input::IsPressed('D')) { transform.angle += (dt * 3.0f); }
+	if (Core::Input::IsPressed('A')) { transform.angle -= (dt * gk::DegreesToRadians(360.0f)); }
+	if (Core::Input::IsPressed('D')) { transform.angle += (dt * gk::DegreesToRadians(360.0f)); }
 	
+	transform.position = gk::Clamp(transform.position, { 0, 0 }, { 800, 600 });
+
 	//Translate
 	//if (Core::Input::IsPressed('A')) { position += gk::Vector2::left * speed * dt; }
 	//if (Core::Input::IsPressed('D')) { position += gk::Vector2::right * speed * dt; }
@@ -62,6 +68,14 @@ void Draw(Core::Graphics& graphics)
 	graphics.DrawString(10, 10, std::to_string(frametime).c_str());
 	graphics.DrawString(10, 20, std::to_string(1.0f / frametime).c_str());
 	graphics.DrawString(10, 30, std::to_string(deltatime / 1000.0f).c_str());
+
+	float v = (std::sin(t) + 1.0f) * 0.5f;
+
+	gk::Color c = gk::Lerp(gk::Color{ 0, 0, 1 }, gk::Color{ 1, 0, 0 }, v);
+	graphics.SetColor(c);
+
+	gk::Vector2 p = gk::Lerp(gk::Vector2{ 400, 300 }, gk::Vector2{ 400, 100 }, v);
+	graphics.DrawString(p.x, p.y, "Last Starfighter");
 
 	if (gameover) graphics.DrawString(400, 300, "Game Over");
 
